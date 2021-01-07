ebc582bd1da656e29259931a13c46044308405b2
Mike McBride <mike.mcbride@hey.com>
2021-01-07 00:32:00 -0600

    add theme builder, update colors

---
+++ config/colors.js
@@ -0,0 +1,33 @@
+module.exports = {
+    transparent: '#ffffff00',
+    nearlyTransparent: '#ffffff09',
+    barelyTransparent: '#ffffffc9',
+    white: '#ffffff',
+    gray: {
+        '10': '#f1f5f9',
+        '20': '#eff2f5',
+        '30': '#e7ebef',
+        '40': '#d4dbe3',
+        '50': '#a8b3bd',
+        '60': '#8b99a7',
+        '70': '#6e7f91',
+        '80': '#424c57',
+    },
+    lightBlue: '#dbeafe',
+    blue: '#2563eb',
+    'blue-10': '#2563eb22',
+    'blue-20': '#2563eb44',
+    'blue-30': '#2563eb53',
+    'blue-40': '#2563eb66',
+    'blue-80': '#2563ebb4',
+    'blue-90': '#2563ebf2',
+    red: '#ef4444',
+    lightRed: '#e11d48',
+    'red-50': '#ef444490',
+    orange: '#ea580c',
+    yellow: '#f59e0b',
+    cyan: '#06b6d4',
+    green: '#10b981',
+    purple: '#7c3aed'
+
+}
---
+++ theme.config.js
@@ -0,0 +1,10 @@
+const colors = require('./config/colors')
+
+module.exports = {
+  name: 'Early Riser',
+  inputFile: 'config/theme.json',
+  outputDir: 'themes',
+  colors,
+  editorBackground: '#f7fafc',
+  borderColor: '#d3dbe4',
+}
